4879990f02eb8f27cfb48adabbb625bf6d7d54e0
Samecoins <146867889+Samecoins@users.noreply.github.com>
2025-11-27 06:15:31 +0400

    ...

---
+++ OrdersStatusMicroservice/Data/FakeDb.cs
@@ -0,0 +1,13 @@
+﻿using OrdersStatusMicroservice.Models;
+
+namespace OrdersStatusMicroservice.Data;
+
+public static class FakeDb
+{
+    public static List<OrderStatus> Statuses { get; } = new()
+    {
+        new OrderStatus { Id = 1, Name = "Доставлен" },
+        new OrderStatus { Id = 2, Name = "Отменён" },
+        new OrderStatus { Id = 3, Name = "Ожидает оплаты" },
+    };
+}
---
+++ OrdersStatusMicroservice/Models/OrderStatus.cs
@@ -0,0 +1,7 @@
+﻿namespace OrdersStatusMicroservice.Models;
+
+public class OrderStatus
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = "";
+}
